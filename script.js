ec802ecebcede926ca31576809ec8fd2f95b5512
RameshNeupane <neupaneramesh555@gmail.com>
2022-02-21 20:42:15 +0545

    watch on the wall

---
+++ script.js
@@ -0,0 +1,30 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
+
+const initialDegree = 90;
+
+hourHand.style.transform = `rotate(${initialDegree}deg)`;
+minuteHand.style.transform = `rotate(${initialDegree}deg)`;
+secondHand.style.transform = `rotate(${initialDegree}deg)`;
+
+const updateTime = () => {
+  const date = new Date();
+
+  const hour = date.getHours() % 12;
+  if (hour === 0) {
+    hour = 12;
+  }
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  let secDegree = (initialDegree + second * 6) % 360;
+  let minDegree = (initialDegree + minute * 6 + second / 10) % 360;
+  let hrDegree = (initialDegree + hour * 30 + minute / 2) % 360;
+
+  secondHand.style.transform = `rotate(${secDegree}deg)`;
+  minuteHand.style.transform = `rotate(${minDegree}deg)`;
+  hourHand.style.transform = `rotate(${hrDegree}deg)`;
+};
+
+setInterval(updateTime, 1000);
